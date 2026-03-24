f9b8472f00a6c5319dfcb673f3ad95a3e567e606
Kvxin <3253261317@qq.com>
2026-03-25 02:12:24 +0800

    feat(style): 添加tailwindcss文件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <header class="header">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
     <h1>Vue 3 SPA Template</h1>
     <nav class="nav">
       <RouterLink to="/">Home</RouterLink>
